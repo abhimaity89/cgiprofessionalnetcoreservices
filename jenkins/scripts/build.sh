769f16b73073cd47a66ebf497cf3953fac22a31a
Ubuntu <ubuntu@ip-172-31-29-198.ap-south-1.compute.internal>
2019-03-28 11:41:40 +0000

    final changes

---
+++ jenkins/scripts/build.sh
@@ -0,0 +1,11 @@
+set -x
+
+dotnet restore
+
+echo "Restore Completed Successfully ... for Build Id ${BUILD_ID}"
+
+dotnet build
+
+echo "Build Successfully Completed ... for Build Id ${BUILD_ID}"
+
+set +x
